b1f3d1c548eeffede47da58bba41ec7941991ddd
Md Mamatajur Rashed <soinik.us.army@gmail.com>
2023-08-10 15:40:22 -0500

    up date Componets and routes file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom"; // Make sure to import Routes and Route
+import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About"; // Import the About component
+import Project from "./routes/Project"; // Import the Project component
+import Contact from "./routes/Contact";
+import Resume from "./routes/Resume"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </>
   );
 }
 
